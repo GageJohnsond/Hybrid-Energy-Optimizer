98c7d2598acbcec90a50e2d388ca5fd4c9f0a1c3
NickSanchez232 <nicksanchez232@gmail.com>
2025-09-13 19:18:34 -0500

    nuke front end

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EnergyDashboard } from "@/components/energy-dashboard"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background grid-pattern">
-      <EnergyDashboard />
-    </main>
-  )
-}
